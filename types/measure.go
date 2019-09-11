94daf26f929414c79c581e08eb4716ddffc5b415
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-11 12:40:57 +0300

    Added DelayLog type.

--- types/measure.go
+++ types/measure.go
@@ -6,5 +6,12 @@ type Measure struct {
 	Name string
 	StartTime int64
 	FinishTime int64
+	PauseStartTime int64
 	TotalTime time.Duration
+	Delaylogs []DelayLog
+}
+
+type DelayLog struct {
+	StartTime int64
+	FinishTime int64
 }
