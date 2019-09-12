278351484ab95836a2bc009a4e6aaa1092677b03
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-12 12:57:44 +0300

    Json names added Measure and DelayLog structs.

--- types/measure.go
+++ types/measure.go
@@ -3,15 +3,17 @@ package types
 import "time"
 
 type Measure struct {
-	Name string
-	StartTime int64
-	FinishTime int64
-	PauseStartTime int64
-	TotalTime time.Duration
-	Delaylogs []DelayLog
+	Name string 			`json:"name"`
+	FirstStartTime int64	`json:"firstStartTime"`
+	StartTime int64			`json:"startTime"`
+	FinishTime int64		`json:"finishTime"`
+	PauseStartTime int64 	`json:"pauseStartTime"`
+	TotalTime time.Duration `json:"totalTime"`
+	Delaylogs []DelayLog	`json:"delayLogs"`
 }
 
 type DelayLog struct {
-	StartTime int64
-	FinishTime int64
+	StartTime int64  `json:"startTime"`
+	FinishTime int64 `json:"finishTime"`
+	TotalTime int64  `json:"totalTime"`
 }
